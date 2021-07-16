76e142c60a0673004d35945189a9f32124b84c5c
Keisukes4n <35905884+Keisukes4n@users.noreply.github.com>
2021-07-16 23:28:10 +0900

    ver.2.1.44
    * Reviewed JavaScript code and json for multilingual.

--- documents/options/_multilingual.js
+++ documents/options/_multilingual.js
@@ -1,80 +1,62 @@
+
+'use strict';
+
 /**
  * module file: _multilingual.js
  * description: 
  */
 
 class multilingual {
-  behaviorWhen;
-  caseOfText;
-  caseOfWebpage;
-  textOpenByNewTab;
-  textOpenByNewWindow;
-  webOpenByNewTab;
-  webOpenByNewWindow;
-  sizeOfNewWindow;
-  specifySizeOfWindow;
-  whenSetWH;
-
-  serviceUsedFor;
-  specifyService;
-  dependingOnSelected;
-
-  translatedLanguage;
-  specifyLanguageCode;
-  ifSetToEn;
-  alphabetIn;
+  behaviorKeys;
+  serviceKeys;
+  languageKeys;
 
   constructor() {
-    this.behaviorWhen        = document.getElementById( 'h2BehaviorWhen' );
-    this.caseOfText          = document.getElementById( 'h3CaseOfText' ); 
-    this.caseOfWebpage       = document.getElementById( 'h3CaseOfWebpage' );
-    this.textOpenByNewTab    = document.getElementById( 'spanTextOpenByNewTab' );
-    this.textOpenByNewWindow = document.getElementById( 'spanTextOpenByNewWindow' );
-    this.webOpenByNewTab     = document.getElementById( 'spanWebpageOpenByNewTab' );
-    this.webOpenByNewWindow  = document.getElementById( 'spanWebpageOpenByNewWindow' );
-    this.sizeOfNewWindow     = document.getElementById( 'h3SizeOfNewWindow' );
-    this.specifySizeOfWindow = document.getElementById( 'spanSpecifySizeOfWindow' );
-    this.whenSetWH           = document.getElementById( 'pWhenSetWH' );
-
-    this.serviceUsedFor      = document.getElementById( 'h2ServiceUsedFor' );
-    this.specifyService      = document.getElementById( 'h3SpecifyTranslationService' );
-    this.dependingOnSelected = document.getElementById( 'pDependingOnSelected' );
-    
-    this.translatedLanguage  = document.getElementById( 'h2TranslatedLanguage' );
-    this.specifyLanguageCode = document.getElementById( 'h3SpecifyLanguageCode' );
-    this.ifSetToEn           = document.getElementById( 'pIfSetToEn' );
-    this.alphabetIn          = document.getElementById( 'pAlphabetIn' );
-
-    this.sectionBehavior();
-    this.sectionService();
-    this.sectionlanguage();
+    this.behaviorKeys = [
+      'h2BehaviorWhen',
+      'h3CaseOfText',
+      'h3CaseOfWebpage',
+      'spanOpenByNewTabText',
+      'spanOpenByNewWindowText',
+      'spanOpenByNewTabWeb',
+      'spanOpenByNewWindowWeb',
+      'h3SizeOfNewWindow',
+      'spanSpecifySizeOfWindow',
+      'pWhenSetWH'
+    ];
+    this.serviceKeys = [
+      'h2ServiceUsedFor',
+      'h3SpecifyTranslationService',
+      'pDependingOnSelected'
+    ];
+    this.languageKeys = [
+      'h2TranslatedLanguage',
+      'h3SpecifyLanguageCode',
+      'pIfSetToEn',
+      'pAlphabetIn'
+    ];
   }
 
-  sectionBehavior() {
-    this.behaviorWhen.textContent        = browser.i18n.getMessage( 'optionPageBehaviorWhen' );
-    this.caseOfText.textContent          = browser.i18n.getMessage( 'optionPageCaseOfText' ); 
-    this.caseOfWebpage.textContent       = browser.i18n.getMessage( 'optionPageCaseOfWebpage' );
-    this.textOpenByNewTab.textContent    = browser.i18n.getMessage( 'optionPageOpenByNewTab' );
-    this.textOpenByNewWindow.textContent = browser.i18n.getMessage( 'optionPageOpenByNewWindow' );
-    this.webOpenByNewTab.textContent     = browser.i18n.getMessage( 'optionPageOpenByNewTab' );
-    this.webOpenByNewWindow.textContent  = browser.i18n.getMessage( 'optionPageOpenByNewWindow' );
-    this.sizeOfNewWindow.textContent     = browser.i18n.getMessage( 'optionPageSizeOfNewWindow' );
-    this.specifySizeOfWindow.textContent = browser.i18n.getMessage( 'optionPageSpecifySizeOfWindow' );
-    this.whenSetWH.textContent           = browser.i18n.getMessage( 'optionPageWhenSetWH' );
+  i18n( array ) {
+    array.forEach( ( key ) => {
+      document.getElementById( key ).textContent = browser.i18n.getMessage( key );
+    });
   }
 
-  sectionService() {
-    this.serviceUsedFor.textContent      = browser.i18n.getMessage( 'optionPageServiceUsedFor' );
-    this.specifyService.textContent      = browser.i18n.getMessage( 'optionPageSpecifyTranslationService' );
-    this.dependingOnSelected.textContent = browser.i18n.getMessage( 'optionPageDependingOnSelected' );
+  getBehaviorKeys() {
+    return this.behaviorKeys;
   }
 
-  sectionlanguage() {
-    this.translatedLanguage.textContent  = browser.i18n.getMessage( 'optionPageTranslatedLanguage' );
-    this.specifyLanguageCode.textContent = browser.i18n.getMessage( 'optionPageSpecifyLanguageCode' )
-    this.ifSetToEn.textContent           = browser.i18n.getMessage( 'optionPageIfSetEn' );
-    this.alphabetIn.textContent          = browser.i18n.getMessage( 'optionPageAlphabetIn' );
+  getServiceKeys() {
+    return this.serviceKeys;
+  }
+
+  getLanguageKeys() {
+    return this.languageKeys;
   }
 }
 
-const multilingualInstance = new multilingual;
+const multilingualIns = new multilingual;
+multilingualIns.i18n( multilingualIns.getBehaviorKeys() );
+multilingualIns.i18n( multilingualIns.getServiceKeys() );
+multilingualIns.i18n( multilingualIns.getLanguageKeys() );
